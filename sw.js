9401df4a4741a7d957d7b9ed8f0c77740742b06a
15001877 <gaozhulin@dongpeng.net>
2026-07-25 11:27:39 +0800

    v16: 基础学习中心（词性/语法/音标）
    - 底部"词性"标签升级为"基础"🎓，内部三子标签切换
    - 词性学习：9类词性各加通俗口诀，朗读追加口诀(TTS)
    - 语法学习(新增)：8个语法点(一般现在/现在进行/过去/将来/完成/比较级/情态动词/There be)，含通俗讲解+结构公式+例句+记忆口诀，TTS朗读
    - 音标学习(新增)：44个音标(12单元音+8双元音+24辅音)，每音标配程序生成SVG口型图+口型描述+2示例词，可朗读示例词(TTS)
    - sw.js 缓存升级至 v16
    - standalone 桌面版同步重建

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "be850-v15";
+const CACHE = "be850-v16";
 const FILES = ["./", "./index.html", "./app.js", "./words.js", "./wordlists.js", "./manifest.json", "./icon.svg", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function (e) {
